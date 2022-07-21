7f3fa3b63cea273c563f67e45e9a490a12f52e03
subv9521 <buisu.humg@gmail.com>
2022-07-22 00:39:24 +0700

    update ngay 22/7

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// header menu mobile //
+let getShowMenu = document.querySelector('.show-menu');
+function showMenu() {
+    getShowMenu.classList.add('open');
+}
+
+function hideShowMenu() {
+    getShowMenu.classList.remove('open')
+}
